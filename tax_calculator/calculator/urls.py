d076fd81e3996c6644b9472396bc0616faf2a563
professor <yongyaatom@gmail.com>
2021-03-22 18:29:45 +0545

    I have added some of the feature where calculator can calculate according to reational status

--- tax_calculator/calculator/urls.py
+++ tax_calculator/calculator/urls.py
@@ -4,7 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('married', views.calc, name='married'),
+    path('unmarried', views.calc, name='unmarried'),
     path('calc', views.calc, name='calc'),
     path('News', views.news, name='news'),
-    path('news_sentence', views.news, name='news_sentence')
 ]
